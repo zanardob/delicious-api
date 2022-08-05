08c765255365aac5c780bf8e88389f734fbd0642
Guilherme Zanardo <guilherme@zanardo.dev>
2022-08-05 17:30:18 -0300

    Add method to create a superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,7 +11,7 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
-        password = 'testpass123'
+        password = 'sample123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
@@ -32,3 +32,18 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'sample123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
