eed72c06bf3d35e3fe8f9658fe47617ade5589d4
zanardo <guilherme@zanardo.dev>
2022-09-16 16:13:55 -0300

    Update model and migrations to allow image upload

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 """
 Tests for models.
 """
+from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -79,3 +80,12 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='Drinks')
 
         self.assertEqual(str(tag), tag.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
